9aeccda19534e1eba82fbbed7b6a026ff02317d7
Diego Mechelynck <diego@agilityc.be>
2019-08-05 15:58:49 +0200

    initialising the project

---
+++ Api.py
@@ -0,0 +1,29 @@
+# Import framework
+from flask import Flask
+from flask_restful import Resource, Api
+
+# Instantiate the app
+app = Flask(__name__)
+api = Api(app)
+
+class Participants(Resource):
+    def get(self):
+        return {
+            'partcipants': ['Jonathan', 'Nicolas', 'Jerome', 'Julien', 'Elise', 'Christophe', 'Joelle', 'Farah', 'Felix', 'Alex', 'Roxane', 'Marc']
+        }
+
+class Professor(Resource):
+    def get(self):
+        return {
+            'professor': ['Laurent']
+        }
+
+# Create routes
+api.add_resource(Participants, '/participants')
+
+# Create routes
+api.add_resource(Professor, '/professor')
+
+# Run the application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=80)
