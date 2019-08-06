98951589dbac0d84633d666b2031a65145263982
Diego Mechelynck <diego@agilityc.be>
2019-08-06 17:09:15 +0200

    made a dummy change

--- Api.py
+++ Api.py
@@ -15,7 +15,7 @@ class Participants(Resource):
 class Professor(Resource):
     def get(self):
         return {
-            'professor': ['Laurent']
+            'professor': ['Diego']
         }
 
 # Create routes
