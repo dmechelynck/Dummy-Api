aa4b5a9d71c2991b17981649895e98a652a074f9
Diego Mechelynck <diego@agilityc.be>
2019-08-06 18:06:49 +0200

    yet another testb of dummy changing something

--- Api.py
+++ Api.py
@@ -15,7 +15,7 @@ class Participants(Resource):
 class Professor(Resource):
     def get(self):
         return {
-            'professor': ['Moi moi']
+            'professor': ['Moi moi et toi']
         }
 
 # Create routes
