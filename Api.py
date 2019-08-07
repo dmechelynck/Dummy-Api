48d8ff3705eb8b6190f92602faf0148cf4a574c3
Diego Mechelynck <diego@agilityc.be>
2019-08-07 12:24:42 +0200

    hello

--- Api.py
+++ Api.py
@@ -15,7 +15,7 @@ class Participants(Resource):
 class Professor(Resource):
     def get(self):
         return {
-            'professor': ['padi diverrrrr hello again']
+            'professor': ['padi diverrrrr hello again again']
         }
 
 # Create routes
