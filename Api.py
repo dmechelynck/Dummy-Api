facc0870f228af7615046eecfc07cd842d286ba3
Diego Mechelynck <diego@agilityc.be>
2019-08-07 12:59:39 +0200

    not yet over

--- Api.py
+++ Api.py
@@ -15,7 +15,7 @@ class Participants(Resource):
 class Professor(Resource):
     def get(self):
         return {
-            'professor': ['testing if sudo kill really works']
+            'professor': ['testing if sudo kill really works h']
         }
 
 # Create routes
