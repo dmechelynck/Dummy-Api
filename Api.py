e80111aed3c0fda84ede4b37a6406c63b8c379fb
Diego Mechelynck <diego@agilityc.be>
2019-08-07 12:55:34 +0200

    yes yes im back

--- Api.py
+++ Api.py
@@ -15,7 +15,7 @@ class Participants(Resource):
 class Professor(Resource):
     def get(self):
         return {
-            'professor': ['with requirents .txt']
+            'professor': ['testing if sudo kill really works']
         }
 
 # Create routes
