c1aa9f67916368f64e918637a125f042453afc95
Diego Mechelynck <diego@agilityc.be>
2019-08-08 14:35:02 +0200

    message added

--- Api.py
+++ Api.py
@@ -18,6 +18,26 @@ class Professor(Resource):
             'professor': ['now I work on airflow']
         }
 
+class TopoHebdo(Resource):
+
+    def get(self):
+        message = """
+        Hello Jon, Chris and Julien. This dummy Api is currently hosted on an Azure virtual machine I launched.
+        The Api is really basic but what is really important is that I applied all the best industrialization practices I learnt at Axa.
+
+        After almost one year at Axa I would like to share what I learned with the rest of the team. Does a training mid-october sound like a good idea?
+
+        Cheers,
+
+        Diego.
+
+        """
+
+        return {
+            'Message': [message],
+            'Topics I would like to tackle': ['Working on linux', "Small github recap" ,'Best practices in deployment using Jenkins',  'Scheduling on linux using Airflow']
+        }
+
 # Create routes
 api.add_resource(Participants, '/participants')
 
