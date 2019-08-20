2de4a4dbaf5915a2b59d0622047d18bf2a2e90fb
Diego Mechelynck <diego@agilityc.be>
2019-08-20 14:57:12 +0200

    testing unit testing inside function

--- Squared.py
+++ Squared.py
@@ -1,5 +1,24 @@
+#### The Functio itself:
+
 def Squared_Function(number):
     return number*number
+
+##### Unit testing:
+
+import unittest
+
+class Test_Squared_Fonctiun(unittest.TestCase):
+    def test1(self):
+        result = Squared_Function(5)
+        self.assertEqual(result, 25)
+
+    def test2(self):
+        result = Squared_Function(2)
+        self.assertEqual(result, 4)
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
 
