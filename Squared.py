8054c239a5ef99672bc39fafe5ba8ad78476b37d
Diego Mechelynck <diego@agilityc.be>
2019-08-20 15:11:21 +0200

    commented error giving test

--- Squared.py
+++ Squared.py
@@ -15,10 +15,12 @@ class Test_Squared_Fonctiun(unittest.TestCase):
     def test2(self):
         result = Squared_Function(2)
         self.assertEqual(result, 4)
-
+"""
     def test3(self):
         result = Squared_Function(2)
         self.assertEqual(result, 5)
+        
+"""
 
 if __name__ == '__main__':
     unittest.main()
